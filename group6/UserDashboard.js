eb243cb9a5b0c6b95ec3e729e42d3dc80e82b1ec
saarfrid <156918978+saarfrid@users.noreply.github.com>
2024-02-21 19:06:53 +0200

    Add files via upload

---
+++ group6/UserDashboard.js
@@ -0,0 +1,11 @@
+// Function to view previous purchases
+function viewPreviousPurchases() {
+
+    window.location.href = "PreviousPurchases.html";
+}
+
+// Function to view liked products
+function viewLikedProducts() {
+
+    window.location.href = "LikedProducts.html";
+}
